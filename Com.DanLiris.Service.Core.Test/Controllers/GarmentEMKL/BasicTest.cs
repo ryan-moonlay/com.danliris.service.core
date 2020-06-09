5633bbab07c2363b56f178f24ee5b3b50eb0e0d7
ANDRI PURNOMO <andrea.kinan@gmail.com>
2020-06-09 10:57:09 +0700

    Create Table Garment EMKL & Trucking
    Create Table Garment EMKL & Trucking

---
+++ Com.DanLiris.Service.Core.Test/Controllers/GarmentEMKL/BasicTest.cs
@@ -0,0 +1,15 @@
+﻿using Com.DanLiris.Service.Core.Lib.Models;
+using Com.DanLiris.Service.Core.Lib.Services.GarmentEMKL;
+using Com.DanLiris.Service.Core.Lib.ViewModels;
+using Com.DanLiris.Service.Core.Test.Utils;
+using Com.DanLiris.Service.Core.WebApi.Controllers.v1.BasicControllers;
+
+namespace Com.DanLiris.Service.Core.Test.Controllers.GarmentEMKL
+{
+    public class BasicTest : BaseControllerTest<GarmentEMKLController, GarmentEMKLModel, GarmentEMKLViewModel, IGarmentEMKLService>
+    {
+        public BasicTest()
+        {
+        }
+    }
+}
---
+++ Com.DanLiris.Service.Core.Test/DataUtils/GarmentLEMKLDataUtil.cs
@@ -0,0 +1,46 @@
+﻿using Com.DanLiris.Service.Core.Lib.Models;
+using Com.DanLiris.Service.Core.Lib.Services.GarmentEMKL;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Com.DanLiris.Service.Core.Test.DataUtils
+{
+    public class GarmentEMKLDataUtil
+    {
+        private readonly GarmentEMKLService Service;
+
+        public GarmentEMKLDataUtil(GarmentEMKLService service)
+        {
+            Service = service;
+        }
+
+        public GarmentEMKLModel GetNewData()
+        {
+            Guid guid = Guid.NewGuid();
+
+            GarmentEMKLModel model = new GarmentEMKLModel
+            {
+                Code = $"Code{guid}",
+                Name = $"Name{guid}",
+                Address = $"Address{guid}",
+                Attention = $"Attention{guid}",
+                PhoneNumber = $"PhoneNumber{guid}",
+                FaxNumber = $"FaxNumber{guid}",
+                Email = $"Email{guid}",
+            };
+
+            return model;
+        }
+
+        public async Task<GarmentEMKLModel> GetTestData()
+        {
+            var data = GetNewData();
+
+            await Service.CreateAsync(data);
+
+            return data;
+        }
+    }
+}
