6266befe40ba241bdd18e0ff4b9524a9b9c3b3f5
adepamungkas <ade.pamungkas9@gmail.com>
2020-08-24 15:38:46 +0700

    QueryHelperTest

---
+++ Com.DanLiris.Service.Core.Test/Helpers/ValidateService/ValidateServiceTest.cs
@@ -0,0 +1,39 @@
+﻿using Com.DanLiris.Service.Core.Lib.Helpers;
+using Com.DanLiris.Service.Core.Lib.ViewModels;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Com.DanLiris.Service.Core.Test.Helpers.ValidateService
+{
+ public   class ValidateServiceTest
+    {
+
+        [Fact]
+        public void Should_Succes_Instantiate_validateService()
+        {
+            var serviceProviderMock = new Mock<IServiceProvider>();
+            var service = new Lib.Helpers.ValidateService.ValidateService(serviceProviderMock.Object);
+
+            AccountBankViewModel viewModel = new AccountBankViewModel();
+            service.Validate(viewModel);
+
+            Assert.NotNull(service);
+        }
+
+
+
+        [Fact]
+        public void Validate_Throws_ServiceValidationExeption()
+        {
+            Mock<IServiceProvider> serviceProvider = new Mock<IServiceProvider>();
+            MachineSpinningProcessTypeViewModel viewModel = new MachineSpinningProcessTypeViewModel();
+
+            var service = new Lib.Helpers.ValidateService.ValidateService(serviceProvider.Object);
+            Assert.Throws<ServiceValidationException>(() => service.Validate(viewModel));
+
+        }
+    }
+}
