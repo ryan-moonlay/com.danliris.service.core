8173e79a37339120594e13433688de65134bd7a7
adepamungkas <ade.pamungkas9@gmail.com>
2020-08-27 17:03:19 +0700

    Update BudgetCurrencyControllerTest.cs

--- Com.DanLiris.Service.Core.Test/Controllers/BudgetCurrency/BudgetCurrencyControllerTest.cs
+++ Com.DanLiris.Service.Core.Test/Controllers/BudgetCurrency/BudgetCurrencyControllerTest.cs
@@ -1,13 +1,20 @@
-﻿using Com.DanLiris.Service.Core.Lib.Helpers.IdentityService;
+﻿using Com.DanLiris.Service.Core.Lib;
+using Com.DanLiris.Service.Core.Lib.Helpers.IdentityService;
 using Com.DanLiris.Service.Core.Lib.Helpers.ValidateService;
 using Com.DanLiris.Service.Core.Lib.Services;
 using Com.DanLiris.Service.Core.WebApi.Controllers.v1.BasicControllers;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.DependencyInjection;
 using Moq;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using System.Net;
+using System.Runtime.CompilerServices;
 using System.Security.Claims;
 using System.Text;
 using Xunit;
@@ -38,26 +45,47 @@ namespace Com.DanLiris.Service.Core.Test.Controllers.BudgetCurrency
             return controller;
         }
 
-        [Fact]
-        public void GetByIds_Return_OK()
+        private CoreDbContext _dbContext(string testName)
         {
-            //Setup
-            Mock<IServiceProvider> serviceProvider = GetServiceProvider();
-            BudgetCurrencyService service =new  BudgetCurrencyService(serviceProvider.Object);
-           
-            //Act
-            IActionResult response = GetController(service).GetByIds(new List<int>() { 1});
+            var serviceProvider = new ServiceCollection()
+              .AddEntityFrameworkInMemoryDatabase()
+              .BuildServiceProvider();
 
-            //Assert
-            int statusCode = this.GetStatusCode(response);
-            Assert.Equal((int)HttpStatusCode.InternalServerError, statusCode);
+            DbContextOptionsBuilder<CoreDbContext> optionsBuilder = new DbContextOptionsBuilder<CoreDbContext>();
+            optionsBuilder
+                .UseInMemoryDatabase(testName)
+                .ConfigureWarnings(w => w.Ignore(InMemoryEventId.TransactionIgnoredWarning))
+                .UseInternalServiceProvider(serviceProvider);
+
+            CoreDbContext dbContext = new CoreDbContext(optionsBuilder.Options);
+
+            return dbContext;
+        }
+
+        protected string GetCurrentAsyncMethod([CallerMemberName] string methodName = "")
+        {
+            var method = new StackTrace()
+                .GetFrames()
+                .Select(frame => frame.GetMethod())
+                .FirstOrDefault(item => item.Name == methodName);
+
+            return method.Name;
+
+        }
+
+        public Lib.Models.BudgetCurrency GetTestData(CoreDbContext dbContext)
+        {
+            Lib.Models.BudgetCurrency data = new Lib.Models.BudgetCurrency();
+            dbContext.BudgetCurrencies.Add(data);
+            dbContext.SaveChanges();
+
+            return data;
         }
 
         protected int GetStatusCode(IActionResult response)
         {
             return (int)response.GetType().GetProperty("StatusCode").GetValue(response, null);
         }
-
 
 
         Mock<IServiceProvider> GetServiceProvider()
@@ -73,5 +101,83 @@ namespace Com.DanLiris.Service.Core.Test.Controllers.BudgetCurrency
               .Returns(validateService.Object);
             return serviceProvider;
         }
+
+        [Fact]
+        public void GetByIds_Return_OK()
+        {
+            //Setup
+            CoreDbContext dbContext = _dbContext(GetCurrentAsyncMethod());
+            Mock<IServiceProvider> serviceProvider = GetServiceProvider();
+            
+            BudgetCurrencyService service =new  BudgetCurrencyService(serviceProvider.Object);
+
+            serviceProvider.Setup(s => s.GetService(typeof(BudgetCurrencyService))).Returns(service);
+            serviceProvider.Setup(s => s.GetService(typeof(CoreDbContext))).Returns(dbContext);
+
+            Lib.Models.BudgetCurrency testData = GetTestData(dbContext);
+
+            //Act
+            IActionResult response = GetController(service).GetByIds(new List<int>() { testData.Id});
+
+            //Assert
+            int statusCode = this.GetStatusCode(response);
+            Assert.Equal((int)HttpStatusCode.OK, statusCode);
+        }
+
+        [Fact]
+        public void GetByIds_Return_InternalServerError()
+        {
+            //Setup
+            Mock<IServiceProvider> serviceProvider = GetServiceProvider();
+            BudgetCurrencyService service = new BudgetCurrencyService(serviceProvider.Object);
+            serviceProvider.Setup(s => s.GetService(typeof(BudgetCurrencyService))).Returns(service);
+            
+            //Act
+            IActionResult response = GetController(service).GetByIds(new List<int>() { 1 });
+
+            //Assert
+            int statusCode = this.GetStatusCode(response);
+            Assert.Equal((int)HttpStatusCode.InternalServerError, statusCode);
+        }
+
+        [Fact]
+        public void GetByName_Return_OK()
+        {
+            //Setup
+            CoreDbContext dbContext = _dbContext(GetCurrentAsyncMethod());
+            Mock<IServiceProvider> serviceProvider = GetServiceProvider();
+
+            BudgetCurrencyService service = new BudgetCurrencyService(serviceProvider.Object);
+
+            serviceProvider.Setup(s => s.GetService(typeof(BudgetCurrencyService))).Returns(service);
+            serviceProvider.Setup(s => s.GetService(typeof(CoreDbContext))).Returns(dbContext);
+
+            Lib.Models.BudgetCurrency testData = GetTestData(dbContext);
+
+            //Act
+            IActionResult response = GetController(service).GetByName("");
+
+            //Assert
+            int statusCode = this.GetStatusCode(response);
+            Assert.Equal((int)HttpStatusCode.OK, statusCode);
+        }
+
+
+        [Fact]
+        public void GetByName_Return_InternalServerError()
+        {
+            //Setup
+            Mock<IServiceProvider> serviceProvider = GetServiceProvider();
+            BudgetCurrencyService service = new BudgetCurrencyService(serviceProvider.Object);
+
+            //Act
+            IActionResult response = GetController(service).GetByName("");
+
+            //Assert
+            int statusCode = this.GetStatusCode(response);
+            Assert.Equal((int)HttpStatusCode.InternalServerError, statusCode);
+        }
+
+        
     }
 }
