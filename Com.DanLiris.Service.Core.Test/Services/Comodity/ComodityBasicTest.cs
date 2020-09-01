287d4db5e8b00de93a78cac8d24c142bfb8da385
adepamungkas <ade.pamungkas9@gmail.com>
2020-09-01 14:08:47 +0700

    comodityServiceTest

--- Com.DanLiris.Service.Core.Test/Services/Comodity/ComodityBasicTest.cs
+++ Com.DanLiris.Service.Core.Test/Services/Comodity/ComodityBasicTest.cs
@@ -1,6 +1,8 @@
 ﻿using Com.Danliris.Service.Core.Test.Helpers;
 using Com.DanLiris.Service.Core.Lib;
 using Com.DanLiris.Service.Core.Lib.Services;
+using Com.DanLiris.Service.Core.Test.DataUtils;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -17,6 +19,17 @@ namespace Com.DanLiris.Service.Core.Test.Services.Comodity
         public ComodityBasicTest(ServiceProviderFixture fixture) : base(fixture, createAttrAssertions, updateAttrAssertions, existAttrCriteria)
         {
         }
+
+        private ComodityServiceDataUtil DataUtil
+        {
+            get { return (ComodityServiceDataUtil)ServiceProvider.GetService(typeof(ComodityServiceDataUtil)); }
+        }
+
+        private ComodityService Services
+        {
+            get { return (ComodityService)ServiceProvider.GetService(typeof(ComodityService)); }
+        }
+
         public override void EmptyCreateModel(Models.Comodity model)
         {
             model.Code = string.Empty;
@@ -38,6 +51,29 @@ namespace Com.DanLiris.Service.Core.Test.Services.Comodity
                 Code = guid,
                 Name = string.Format("TEST {0}", guid),
             };
+        }
+
+        [Fact]
+        public void Should_Success_MapToViewModel()
+        {
+            var model = DataUtil.GetNewData();
+            var Response = Services.MapToViewModel(model);
+            Assert.NotNull(Response);
+        }
+
+        [Fact]
+        public async void Should_Success_ReadModel()
+        {
+           var model = await DataUtil.GetTestBuget();
+
+            var orderData = new
+            {
+                Code = "asc"
+            };
+            string order = JsonConvert.SerializeObject(orderData);
+
+            var Response = Services.ReadModel(1, 25, order, new List<string>(), "", "{}");
+            Assert.NotNull(Response);
         }
     }
 }
