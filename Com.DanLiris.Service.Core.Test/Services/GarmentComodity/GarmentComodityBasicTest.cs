f2a067aeba2e30c6940d2687362f6b1c8017e493
adepamungkas <ade.pamungkas9@gmail.com>
2020-09-03 07:32:44 +0700

    Update GarmentComodityBasicTest.cs

--- Com.DanLiris.Service.Core.Test/Services/GarmentComodity/GarmentComodityBasicTest.cs
+++ Com.DanLiris.Service.Core.Test/Services/GarmentComodity/GarmentComodityBasicTest.cs
@@ -1,7 +1,10 @@
 ﻿using Com.Danliris.Service.Core.Test.Helpers;
 using Com.DanLiris.Service.Core.Lib;
 using Com.DanLiris.Service.Core.Lib.Services;
+using Com.DanLiris.Service.Core.Test.DataUtils;
+using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 using Xunit;
 using Models = Com.DanLiris.Service.Core.Lib.Models;
 
@@ -21,6 +24,12 @@ namespace Com.DanLiris.Service.Core.Test.Services.GarmentComodity
         {
             get { return (GarmentComodityService)ServiceProvider.GetService(typeof(GarmentComodityService)); }
         }
+
+        private GarmentComodityDataUtil DataUtil
+        {
+            get { return (GarmentComodityDataUtil)ServiceProvider.GetService(typeof(GarmentComodityDataUtil)); }
+        }
+
         public override void EmptyCreateModel(Models.GarmentComodity model)
         {
             model.Code = string.Empty;
@@ -52,6 +61,21 @@ namespace Com.DanLiris.Service.Core.Test.Services.GarmentComodity
             var Response = Services.MapToViewModel(model);
             Assert.NotNull(Response);
             
+        }
+
+        [Fact]
+        public async void Should_Success_ReadModel()
+        {
+            var model = await DataUtil.GetTestDataAsync();
+
+            var orderData = new
+            {
+                Code = "asc"
+            };
+            string order = JsonConvert.SerializeObject(orderData);
+
+            var Response = Services.ReadModel(1, 25, order, new List<string>(), "", "{}");
+            Assert.NotNull(Response);
         }
     }
 }
