f92275122a90ae808a7a8e8d0d65715d4ea08bcd
adepamungkas <ade.pamungkas9@gmail.com>
2020-09-03 11:35:09 +0700

    Unit Test Model

---
+++ Com.DanLiris.Service.Core.Test/ViewModels/Account_and_Roles/AccountProfileViewModelTest.cs
@@ -0,0 +1,25 @@
+﻿using Com.DanLiris.Service.Core.Lib.ViewModels.Account_and_Roles;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Com.DanLiris.Service.Core.Test.ViewModels.Account_and_Roles
+{
+  public  class AccountProfileViewModelTest
+    {
+        [Fact]
+        public void Should_Success_Instantiate_ProductDesignViewModel()
+        {
+            AccountProfileViewModel viewModel = new AccountProfileViewModel()
+            {
+                Firstname= "Firstname",
+                Gender= "Gender",
+                Lastname = "Lastname"
+            };
+            Assert.Equal("Gender", viewModel.Gender);
+            Assert.Equal("Firstname", viewModel.Firstname);
+            Assert.Equal("Lastname", viewModel.Lastname);
+        }
+    }
+}
---
+++ Com.DanLiris.Service.Core.Test/ViewModels/Account_and_Roles/AccountRoleViewModelTest.cs
@@ -0,0 +1,32 @@
+﻿using Com.DanLiris.Service.Core.Lib.Models.Account_and_Roles;
+using Com.DanLiris.Service.Core.Lib.ViewModels.Account_and_Roles;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Com.DanLiris.Service.Core.Test.ViewModels.Account_and_Roles
+{
+  public  class AccountRoleViewModelTest
+    {
+
+        [Fact]
+        public void Should_Success_Instantiate()
+        {
+            AccountRoleViewModel viewModel = new AccountRoleViewModel()
+            {
+                AccountId=1,
+                Id=1,
+                Role=new Role()
+                {
+                    Code="Code"
+                },
+                RoleId=1
+            };
+            Assert.Equal(1, viewModel.AccountId);
+            Assert.Equal(1, viewModel.Id);
+            Assert.Equal(1, viewModel.RoleId);
+            Assert.NotNull(viewModel.Role);
+        }
+    }
+}
