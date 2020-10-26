0c0fc339a41b59e2225ce25e19458b383f71efae
adepamungkas <ade.pamungkas9@gmail.com>
2020-10-26 14:22:52 +0700

    Adding Unit OrderTypes

--- Com.DanLiris.Service.Core.Lib/Migrations/20201026040153_Adding_Unit_OrderTypes.Designer.cs
+++ Com.DanLiris.Service.Core.Lib/Migrations/20201026040153_Adding_Unit_OrderTypes.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace Com.DanLiris.Service.Core.Lib.Migrations
 {
     [DbContext(typeof(CoreDbContext))]
-    partial class CoreDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201026040153_Adding_Unit_OrderTypes")]
+    partial class Adding_Unit_OrderTypes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
